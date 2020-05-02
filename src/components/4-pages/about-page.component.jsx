796e4d03ed15d47e7f8d25d05621d52954cfe591
James Walker <jamesmawalker@gmail.com>
2020-05-02 15:06:03 +0700

    Add AboutPage content and styling.

--- src/components/4-pages/about-page.component.jsx
+++ src/components/4-pages/about-page.component.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../Icons/logo-icon.svg';
+import { ReactComponent as Divider } from '../../Icons/title-divider.svg';
 
 class AboutPage extends Component {
   render() {
     return (
-      <div>
-        <h1>ABOUT NISHELLE</h1>
+      <div className='about'>
+        <div className='about__info'>
+          <Link>
+            <Logo className='about__logo' to='/' />
+          </Link>
+          <span className='about__header-section'>
+            <h1 className='about__heading'>About Nishelle</h1>
+            <Divider className='divider' />
+          </span>
+          <div className='about__text'>
+            Nishelle loves to tell stories... through her photography. Her
+            stories are based upon daydreams, reality and often a mixture of the
+            two - depending on how she see's fit. Through her camera she
+            expresses the inner world of both herself and her subjects. Together
+            they make magic!
+          </div>
+        </div>
+        <div className='about__photo-section'>
+          <img
+            src='https://res.cloudinary.com/jameswalker-work/image/upload/f_auto/q_auto/v1588406318/NW_Photography/nisa-flowers_vcgvgc.jpg'
+            alt=''
+            className='about__photo'
+          />
+        </div>
       </div>
     );
   }
